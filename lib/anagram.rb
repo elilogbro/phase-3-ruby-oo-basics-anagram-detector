6e39581a16b03baaeb1f0373fd6e55820fe6a16a
Eli <+>
2022-08-29 20:32:00 -0400

    commit tree a830ea7b33efeb435d192e8aa16db0986e2e3b6f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_reader :word
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def array=(value)
+        @array = value
+    end
+
+    def match
+        word_array = @word.chars
+        # iterate over array => filter
+        filtered = @array.filter do |words|
+            word_array.sort == words.sort
+        end
+
+        puts filtered
+    end
+
+end
+
+# binding.pry
